5233def7f7ed6456c37d45e492bc31e6ad39b25b
k32726bj <benjamin.james-3@student.manchester.ac.uk>
2024-11-14 19:56:08 +0000

    added gitignore, combined train files into one

--- src/train.py
+++ src/train.py
@@ -1,21 +1,20 @@
+import sys
 import time
-import torch
 import numpy as np
-from torch import nn
+from torch import nn, no_grad, float, save, optim
 from torch.utils.data import DataLoader
 from torchvision import datasets
-from torchvision.transforms import ToTensor, Compose, Normalize, Pad, RandomCrop, RandomHorizontalFlip, Resize
-import alexnet
+from torchvision.transforms import ToTensor, Compose, Normalize, Pad, RandomCrop, RandomHorizontalFlip
+import lenet, resnet
 
 DATASET_PATH = "../datasets/cifar10"
 MODELS_PATH = "../models/"
 
 def get_data(batch_size):
     train_transform = Compose([
-        # Pad(4),
-        # RandomCrop(32),
-        # RandomHorizontalFlip(),
-        Resize((224, 224)),
+        Pad(4),
+        RandomCrop(32),
+        RandomHorizontalFlip(),
         ToTensor(),
         Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2470, 0.2435, 0.2616])
     ])
@@ -25,7 +24,6 @@ def get_data(batch_size):
         Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2470, 0.2435, 0.2616])
     ])
 
-    # Download training data from open datasets.
     training_data = datasets.CIFAR10(
         root=DATASET_PATH,
         train=True,
@@ -33,7 +31,6 @@ def get_data(batch_size):
         transform=train_transform,
     )
 
-    # Download test data from open datasets.
     test_data = datasets.CIFAR10(
         root=DATASET_PATH,
         train=False,
@@ -60,7 +57,7 @@ def train(dataloader, model, loss_function, optimizer):
         optimizer.step()
 
         train_loss += loss.item()
-        train_acc += (pred.argmax(1) == y).type(torch.float).sum().item()
+        train_acc += (pred.argmax(1) == y).type(float).sum().item()
 
     train_loss /= len(dataloader)
     train_acc /= len(dataloader.dataset)
@@ -69,7 +66,7 @@ def train(dataloader, model, loss_function, optimizer):
 def test(dataloader, model, loss_function):
     model.eval()
     test_loss, test_acc = 0, 0
-    with torch.no_grad():
+    with no_grad():
         for X, y in dataloader:
             X, y = X.to("cpu"), y.to("cpu")
 
@@ -77,38 +74,57 @@ def test(dataloader, model, loss_function):
             loss = loss_function(pred, y)
 
             test_loss += loss.item()
-            test_acc += (pred.argmax(1) == y).type(torch.float).sum().item()
+            test_acc += (pred.argmax(1) == y).type(float).sum().item()
 
     test_loss /= len(dataloader)
     test_acc /= len(dataloader.dataset)
     print(f"Test Accuracy: {(100*test_acc):>0.1f}%, Test loss: {test_loss:>8f}")
     return test_acc, test_loss
 
-def save_model(filename):
+def save_model(model, filename):
     filepath = MODELS_PATH + filename
-    torch.save(model.state_dict(), filepath)
+    save(model.state_dict(), filepath)
     print(f"Saved PyTorch Model State to {filepath}")
 
-train_dataloader, test_dataloader = get_data(batch_size=32)
-model = alexnet.AlexNet()
-loss_function = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+def process(model, model_filename, epochs):
+    train_dataloader, test_dataloader = get_data(batch_size=32)
+    loss_function = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=5e-4)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
 
-epochs = 16
-stats = np.zeros((3, epochs)) # accuracy, loss, time
-for epoch in range(epochs):
-    start = time.time()
-    print(f"----------------- EPOCH {epoch+1} --------------")
+    stats = np.zeros((3, epochs)) # accuracy, loss, time
+    for epoch in range(epochs):
+        start = time.time()
+        print(f"----------------- EPOCH {epoch+1} --------------")
 
-    train(train_dataloader, model, loss_function, optimizer)
-    stats[0][epoch], stats[1][epoch] = test(test_dataloader, model, loss_function)
-    scheduler.step()
+        train(train_dataloader, model, loss_function, optimizer)
+        stats[0][epoch], stats[1][epoch] = test(test_dataloader, model, loss_function)
+        scheduler.step()
 
-    end = time.time()
-    stats[2][epoch] = end - start
-    print(f"{stats[2][epoch]:.2f}s")
+        end = time.time()
+        stats[2][epoch] = end - start
+        print(f"{stats[2][epoch]:.2f}s")
 
-total_time = np.sum(stats[2])
-print(f"Finished in {total_time//60:.0f}mins{total_time%60:.0f}s")
-save_model("resnet18.pth")
+    total_time = np.sum(stats[2])
+    print(f"Finished in {total_time//60:.0f}mins{total_time%60:.0f}s")
+    save_model(model, model_filename)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Incorrect number of arguments")
+        sys.exit()
+    model_name = sys.argv[1]
+    epochs = int(sys.argv[2])
+    model = None
+    if model_name == "lenet":
+        model = lenet.LeNet()
+    elif model_name == "resnet9":
+        model = resnet.ResNet9()
+    elif model_name == "resnet18":
+        model = resnet.ResNet18()
+    else:
+        print("Incorrect model name, options: lenet, resnet9, resnet18")
+        sys.exit()
+    filename = model_name + ".pth"
+    process(model, filename, epochs)
+    
