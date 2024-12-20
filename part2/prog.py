abed0d59f134d547f2b0ca1046abf81e20cb83fa
k32726bj <benjamin.james-3@student.manchester.ac.uk>
2024-12-20 15:23:47 +0000

    reset to working algorithm

--- part2/prog.py
+++ part2/prog.py
@@ -37,46 +37,35 @@ class MACCell:
     def mult(self, weights):
         self.accumulator = np.sum(self.weights * weights)
 
-class DirectConv2d(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1):
-        super(DirectConv2d, self).__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding = padding
-        self.dilation = dilation
+def atomic(input_data, pos, depth):
+    # broadcast the input data and multiply and accumulate for each cell
+    mac_array.broadcast(input_data[pos][64*depth:64*(depth+1)])
+    return mac_array.combine()
 
-    def atomic(self, input_data, pos, depth):
-        # broadcast the input data and multiply and accumulate for each cell
-        mac_array.broadcast(input_data[pos][64*depth:64*(depth+1)])
-        return mac_array.combine()
+def stripe(input_data, output_shape, depth, kpos):
+    result = np.empty(output_shape)
+    for oy in range(output_shape[1]):
+        for ox in range(output_shape[0]):
+            ix = ox + kpos[0]
+            iy = oy + kpos[1]
+            if ix < input_data.shape[0] and iy < input_data.shape[1]:
+                result[ox, oy] = atomic(input_data, (ix, iy), depth)
+    return result
 
-    def stripe(self, input_data, output_shape, depth, kpos):
-        result = np.empty(output_shape)
-        for oy in range(output_shape[1]):
-            for ox in range(output_shape[0]):
-                ix = ox + kpos[0]
-                iy = oy + kpos[1]
-                if ix < input_data.shape[0] and iy < input_data.shape[1]:
-                    result[ox, oy] = atomic(input_data, (ix, iy), depth)
-        return result
+def block(input_data, kernels, output_shape, depth):
+    acc = np.zeros(output_shape)
+    for y in range(kernels.shape[2]):
+        for x in range(kernels.shape[1]):
+            load_kernels(kernels, (x, y, depth))
+            acc += stripe(input_data, output_shape, depth, (x, y))
+    return acc
 
-    def block(self, input_data, kernels, output_shape, depth):
-        acc = np.zeros(output_shape)
-        for y in range(kernels.shape[2]):
-            for x in range(kernels.shape[1]):
-                load_kernels(kernels, (x, y, depth))
-                acc += stripe(input_data, output_shape, depth, (x, y))
-        return acc
-
-    def channel(self, input_data, kernels, output_shape):
-        block_ops = int((input_data.shape[-1] + 63) / 64)
-        blocks = np.zeros(output_shape, dtype=np.float64)
-        for depth in range(block_ops):
-            blocks += block(input_data, kernels, output_shape, depth)
-        return blocks
-
+def channel(input_data, kernels, output_shape):
+    block_ops = int((input_data.shape[-1] + 63) / 64)
+    blocks = np.zeros(output_shape, dtype=np.float64)
+    for depth in range(block_ops):
+        blocks += block(input_data, kernels, output_shape, depth)
+    return blocks
 
 def load_kernels(kernels, pos): # e.g. (x, y, z)
     # cache kernels into macs
